d4edce434c50c63eee0981d114095c3f201d972c
Weilin <100191575+weilin1205@users.noreply.github.com>
2022-06-02 18:37:30 +0800

    Create 10130-SuperSale.cpp

---
+++ 10130-SuperSale/10130-SuperSale.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <algorithm>
+
+int T, N, G, MW;
+int price[1005], weight[1005];
+int dp[35];
+int Knapsack();
+
+int main()
+{
+    scanf("%d", &T);
+    while (T--) {
+        // Input
+        scanf("%d", &N);
+        for (int i = 0; i < N; ++i)
+            scanf("%d %d", &price[i], &weight[i]);
+        scanf("%d", &G);
+
+        std::fill(std::begin(dp), std::end(dp), 0);
+        MW = 30;
+        Knapsack();
+
+        int result = 0;
+        while (G--) {
+            scanf("%d", &MW);
+            result += Knapsack();
+        }
+        printf("%d\n", result);
+    }
+}
+int Knapsack()
+{
+    if (dp[MW]) return dp[MW];
+    int dp[35] = {0};
+    for (int i = 0; i < N; ++i) {
+        for (int j = MW; j-weight[i] >= 0; --j) {
+            dp[j] = std::max(dp[j], dp[j-weight[i]] + price[i]);
+        }
+    }
+    return dp[MW];
+}
